c0908db0287c1c850a93f0a4a331be3169983ea0
narenkhatwani <48763124+narenkhatwani@users.noreply.github.com>
2021-11-08 19:45:32 -0500

    Update cheat_sheet.py

--- cheat_sheet.py
+++ cheat_sheet.py
@@ -13,4 +13,75 @@ st.set_page_config(
 st.title("Arkouda Cheat Sheet")
 
 
+def main():
+    cs_sidebar()
+    cs_body()
 
+    return None
+
+
+def cs_sidebar():
+
+    st.sidebar.markdown('''[<img src='data:image/png;base64,{}' class='img-fluid' width=32 height=32>](https://streamlit.io/)'''.format(img_to_bytes("logomark_website.png")), unsafe_allow_html=True)
+    st.sidebar.header('Streamlit cheat sheet')
+
+    st.sidebar.markdown('''
+<small>Summary of the [docs](https://docs.streamlit.io/en/stable/api.html), as of [Streamlit v1.0.0](https://www.streamlit.io/).</small>
+    ''', unsafe_allow_html=True)
+
+    return None
+
+def cs_body():
+    # Magic commands
+
+    col1, col2, col3 = st.columns(3)
+
+    col1.subheader('Magic commands')
+    col1.code('''# Magic commands implicitly `st.write()`
+\'\'\' _This_ is some __Markdown__ \'\'\'
+a=3
+'dataframe:', data
+    ''')
+
+    # Display text
+
+    col1.subheader('Display text')
+    col1.code('''
+st.text('Fixed width text')
+st.markdown('_Markdown_') # see *
+st.caption('Balloons. Hundreds of them...')
+st.latex(r\'\'\' e^{i\pi} + 1 = 0 \'\'\')
+st.write('Most objects') # df, err, func, keras!
+st.write(['st', 'is <', 3]) # see *
+st.title('My title')
+st.header('My header')
+st.subheader('My sub')
+st.code('for i in range(8): foo()')
+* optional kwarg unsafe_allow_html = True
+    ''')
+
+    # Display data
+
+    col1.subheader('Display data')
+    col1.code('''
+st.dataframe(my_dataframe)
+st.table(data.iloc[0:10])
+st.json({'foo':'bar','fu':'ba'})
+st.metric(label="Temp", value="273 K", delta="1.2 K")
+    ''')
+
+    # Display media
+
+    col1.subheader('Display media')
+    col1.code('''
+st.image('./header.png')
+st.audio(data)
+st.video(data)
+    ''')
+
+    return None
+
+# Run main()
+
+if __name__ == '__main__':
+    main()
